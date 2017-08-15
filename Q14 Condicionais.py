ae5377398ebbd623ae736676d83aea0839c93f9d
DanPsousa <pab.324@outlook.com>
2017-08-15 12:02:55 -0300

    Delete Q14 Condicionais.py

--- Q14 Condicionais.py
+++
@@ -1,7 +0,0 @@
-def Lei(data):
-    n= 2017-data
-    if n>=18:
-        return "Pode votar nas proximas eleições para prefeito"
-    else:
-        return "Não pode votar nas proximas eleições para prefeito"
-print(Lei(1999))
