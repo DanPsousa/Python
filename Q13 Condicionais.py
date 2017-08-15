7f0aba4990beea170233e1f986622e3c99de7965
DanPsousa <pab.324@outlook.com>
2017-08-15 12:03:05 -0300

    Delete Q13 Condicionais.py

--- Q13 Condicionais.py
+++
@@ -1,6 +0,0 @@
-def Par(v):
-    if (v%2)==0 and (v%3)==0:
-        return "O numero é par divisivel por 3"
-    else:
-        return "O numero não esta entre as condições necessarias"
-print(Par(15))
